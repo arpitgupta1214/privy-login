7bf493b115dbe1a666774160f528b82f16ab315d
Arpit Gupta <arpitgupta1214@gmail.com>
2024-11-24 21:04:16 +0530

    add logging

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,5 @@ import { base, baseSepolia } from "viem/chains";
 
 const IS_PROD = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
 export const CHAIN = IS_PROD ? base : baseSepolia;
+
+console.log({ IS_PROD, CHAIN })
